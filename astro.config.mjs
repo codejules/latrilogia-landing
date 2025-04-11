4bea68ce747adae455ab759d688e3f8c8e7b0fd7
Julio Serra <julioserra@protonmail.com>
2025-04-11 08:58:08 +0200

    Uninstall tailwind dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify';
 import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()],
+  integrations: [preact()],
   output: 'server',
   adapter: netlify(),
   vite: {
