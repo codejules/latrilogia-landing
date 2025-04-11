5bea0a6c574ab7fbdc8d93fd6d86d9ed855bda66
Julio Serra <julioserra@protonmail.com>
2025-04-11 10:00:50 +0200

    Update dependencies and work done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify';
 import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
+  integrations: [tailwind(), preact()],
   output: 'server',
   adapter: netlify(),
   vite: {
