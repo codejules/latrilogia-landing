8e8d74fcc7e7f7d8389a9d05b651c4c93e89499d
Julio Serra <julioserra@protonmail.com>
2025-04-23 13:47:22 +0200

    Add type module and update path in scripts

--- src/utils/nonceCSP.ts
+++ src/utils/nonceCSP.ts
@@ -1,14 +1,12 @@
 import type { AstroGlobal } from 'astro';
 
 export function nonceCSP(Astro: AstroGlobal) {
-	const nonce = crypto.randomUUID().replace(/-/g, ""); // Generamos el nonce
+    const nonce = crypto.randomUUID().replace(/-/g, "");
 
-	// Configuramos Content Security Policy para la respuesta
-	Astro.response.headers.set(
-		'Content-Security-Policy',
-		`default-src 'self'; script-src 'self' 'nonce-${nonce}' https://js.hcaptcha.com https://newassets.hcaptcha.com https://cdn.jsdelivr.net https://web3forms.com; style-src 'self' 'unsafe-inline'; font-src 'self'; object-src 'none'; frame-src https://newassets.hcaptcha.com; connect-src 'self' https://newassets.hcaptcha.com; img-src 'self';`
-	);
+    Astro.response.headers.set(
+        'Content-Security-Policy',
+        `default-src 'self'; script-src 'self' 'nonce-${nonce}' https://js.hcaptcha.com https://newassets.hcaptcha.com https://cdn.jsdelivr.net https://web3forms.com; style-src 'self' 'unsafe-inline'; font-src 'self'; object-src 'none'; frame-src https://newassets.hcaptcha.com; connect-src 'self' https://newassets.hcaptcha.com; img-src 'self';`
+    );
 
-	// Retornamos el nonce para que pueda ser usado en el layout y otros componentes
-	return { nonce };
+    return { nonce };
 }
